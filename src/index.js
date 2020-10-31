7fa1154beb4445d654c4d90eb4e1f22084482be6
Stefan Dudu <dudu.steffan@yahoo.com>
2020-10-31 15:43:49 +0200

    cleaning up the project, moving to VS code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<React.StrictMode>
+
+	<div>
+    <Card />
+    <Card />
+    <Card />
+    </div>
+
+
+  </React.StrictMode>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/untitled.js
+++ src/untitled.js
@@ -3,10 +3,11 @@ fetch('http://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c
 	.then(data => console.log(data.main.temp))
 
 const urls = [
-'http://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=paris',
-'http://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=berlin',
-'http://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=bucharest'
+'https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=paris',
+'https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=berlin',
+'https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=bucharest'
 ]
+
 Promise.all(urls.map(url => {
 	return fetch(url).then(resp => resp.json())
 })).then(results => {
@@ -14,3 +15,27 @@ Promise.all(urls.map(url => {
 	console.log(results[1].main.temp)
 	console.log(results[2].main.temp)
 }).catch(() => console.log('something went wrong'))
+
+var arry = [
+	return Promise.all(urls.map(url => {
+	return fetch(url).then(resp => resp.json())
+})).then(results => {
+	console.log(results[0])
+	console.log(results[1])
+	console.log(results[2])
+	return results
+}).catch(() => console.log('something went wrong'))
+]
+
+
+// this works
+var array = [
+Promise.all(urls.map(url => {
+    return fetch(url).then(resp => resp.json())
+})).then(results => {
+    console.log(results[0])
+    console.log(results[1])
+    console.log(results[2])
+    return results[0]
+}).catch(() => console.log('something went wrong'))
+]
