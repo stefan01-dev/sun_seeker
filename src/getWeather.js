4a2f6e39ce0904e1648fcf68f74b57a7bcd9c456
stefan01-dev <stefan01.dev@gmail.com>
2020-12-05 22:46:45 +0200

    no more erro showing but still not laoding the city name

--- src/getWeather.js
+++ src/getWeather.js
@@ -6,15 +6,18 @@ const urls = [
 'https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=bucharest'
 ]
 
-  var getWeather =
-       {/* this works, got to run in in chrome console */}
-      Promise.all(urls.map(url => {
+  const getWeather = () => {
+    return(
+      <div>
+    {/* this works, got to run in in chrome console */}
+      { Promise.all(urls.map(url => {
           return fetch(url).then(resp => resp.json())
       })).then(results => {
         //Returns the name of the city
           console.log(results[2].name)
           return results[0]
-      }).catch(() => console.log('something went wrong'))
-
-
+      }).catch(() => console.log('something went wrong'))}
+      </div>
+    )
+    }
 export default getWeather;
