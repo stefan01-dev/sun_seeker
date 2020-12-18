96164718ad8ae5fd2918733a99a59cfa4440bf73
stefan01-dev <stefan01.dev@gmail.com>
2020-12-18 06:25:53 +0200

    XML

--- src/Card.js
+++ src/Card.js
@@ -6,7 +6,7 @@ const Card = () => {
 		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 		 <img alt='' src='https://robohash.org/test?200x200' />
 		  <div>
-			<h2> {getWeather} </h2>
+			<h2 id='demo'></h2>
 			<p> Temperature </p>
 		  </div>
 		</div>
