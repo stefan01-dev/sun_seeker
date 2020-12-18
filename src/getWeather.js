bf70c90f0945b41a53fffc30137e32314fc939cb
stefan01-dev <stefan01.dev@gmail.com>
2020-12-18 06:31:39 +0200

    fetching data from JSON

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,23 +1,13 @@
 import React from 'react';
 
-const urls = [
-'https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=paris',
-'https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=berlin',
-'https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=bucharest'
-]
+var getWeather = new XMLHttpRequest();
+getWeather.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    var myObj = JSON.parse(this.responseText);
+    document.getElementById("demo").innerHTML = myObj.main.temp;
+  }
+};
+getWeather.open("GET", "https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=bucharest", true);
+getWeather.send();
 
-  const getWeather = () => {
-    return(
-      <div>
-      {/* this works, got to run in in chrome console */}
-      { Promise.all(urls.map(url => {
-          return fetch(url).then(resp => resp.json())
-      })).then(results => {
-        //Returns the name of the city
-          console.log(results[2].name)
-          return results[2].name
-      }).catch(() => console.log('something went wrong'))}
-      </div>
-    )
-    }
 export default getWeather;
