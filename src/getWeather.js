08758dfe97071496c79c4c07021cc0c427734e73
stefan01-dev <stefan01.dev@gmail.com>
2021-01-17 18:57:54 +0200

    update2

--- src/getWeather.js
+++ src/getWeather.js
@@ -14,9 +14,13 @@ getWeather.onreadystatechange = function() {
 getWeather.open("GET", "https://api.openweathermap.org/data/2.5/weather?appid=e66e299afb2ec32c804c326c2d169257&units=metric&q=bucharest", true);
 getWeather.send();
 
+
+//  Change the way of API retrive data
 async function getLocationWeather(location) {
   const result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`);
   return result.json();
 }
+
+await getLocationWeather("London");
 
 export default getWeather;
