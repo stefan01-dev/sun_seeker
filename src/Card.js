ec3b86510b0c95f953506f282bdd9b1f4f17768c
stefan01-dev <stefan01.dev@gmail.com>
2021-03-04 05:07:32 +0200

    working on getting api fetch

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,13 @@
 import React from 'react';
-//import getWeather from './getWeather';
+import getWeather from './getWeather';
 
 const Card = () => {
 	return (
 		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
 		 <img alt='' src='https://robohash.org/test?200x200' />
 		  <div>
-			<h2 id='cityName'></h2>
+			<h2 id='cityName'> City</h2>
 			<h3 id='demo'> degrees</h3>
-			<p> Temperature </p>
 		  </div>
 		</div>
 		);
